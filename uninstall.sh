09c7386b1abe799ce5e0447ef1d6642534e1beb3
Alan Hamlett <alan.hamlett@gmail.com>
2022-05-24 19:02:45 -0700

    improve uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -15,12 +15,12 @@ rm -rf "${PLUGINS_DIR}/Alcatraz.xcplugin"
 
 # Delete WakaTime config files from $HOME folder
 echo "Deleting WakaTime config and log files..."
-rm $HOME/.wakatime.* || true
+rm -rf $HOME/.wakatime*
 
 echo "Deleting self-signed cert from default keychain..."
 security delete-identity -c XcodeSigner2018
 
 echo "Finished uninstalling WakaTime."
-echo "Optionally, delete <code>XcodeWithPlugins.app</code> from your <code>~/Applications</code> folder if you ran install with the copy argument."
+echo "Optionally, delete <code>/Applications/XcodeWithPlugins.app</code> if you installed with the copy argument."
 
 exit 0
